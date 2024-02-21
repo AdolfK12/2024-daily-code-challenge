af7c54037a3bc469d30ecf54a76e757dfe4b14e4
AdolfK12 <adolfkevinb@gmail.com>
2024-02-21 23:50:50 +0700

    Day 4

---
+++ 4-Is he gonna survive.js
@@ -0,0 +1,9 @@
+// function hero(bullets, dragons) {
+//   return bullets / 2 >= dragons;
+// }
+
+//function expression
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
+
+console.log(hero(10, 5));
+console.log(hero(1500, 751));
